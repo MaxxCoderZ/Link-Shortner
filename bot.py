2e17398151cf8417ea87b5f0eaed666c09b425f0
Mᴀxx ⚡ <84794903+MaxxCoderZ@users.noreply.github.com>
2021-05-27 09:28:01 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,37 @@
+from os import environ
+import aiohttp
+from pyrogram import Client, filters
+
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+API_KEY = environ.get('API_KEY', '5fd20df0c4db85798dd4f5ff3d03e3606a94f98b')
+
+bot = Client('Link shortner',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=50,
+             sleep_threshold=10)
+
+@bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(bot, message):
+    link = message.matches[0].group(0)
+    try:
+        short_link = await get_shortlink(link)
+        await message.reply(f'Here is your👉 [Short Link🎈]({short_link})', quote=True)
+    except Exception as e:
+        await message.reply(f'Error: {e}', quote=True)
+
+
+async def get_shortlink(link):
+    url = 'https://gplinks.in/api'
+    params = {'api': API_KEY, 'url': link}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
+
+
+bot.run()
