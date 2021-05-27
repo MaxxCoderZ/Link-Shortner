cd74199b19a7594d3325a1994cd235c6da4131ef
Mᴀxx ⚡ <84794903+MaxxCoderZ@users.noreply.github.com>
2021-05-27 09:03:25 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,23 @@
+class script(object):
+
+    START_MSG = """ <b>Hai {},
+
+I'm a simple link shortner Bot which can convert Your link to a shortern Form 💯
+
+For more click help..
+
+You must join our channel in order to use me 😇</b>"""
+    HELP_MSG = """Hai, Follow these Steps..
+
+<code>🌀 Send me any link to shortern..</code>
+
+<code>🌀 Your Shortern link will be Generated and uploaded</code>"""
+    ABOUT_MSG = """⭕️<b>My Name : CoderZ Link Shortner</b>
+
+⭕️<b>Language :</b> <code>Python3</code>
+
+⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+
+⭕️<b>Creator :</b> <a href='Https://t.me/CoderzHEX'>CoderzHEX</a>
+
+⭕<b>Source Code :</b> 👉 <a href='http://t.me/nokkiirunnoippokittum'>click here</a>"""
