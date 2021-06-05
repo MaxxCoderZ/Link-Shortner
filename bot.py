942d486fdcc02f87def61b80476c118f7772818f
Vɪᴠᴇᴋ <Vivektvp1@gmail.com>
2021-06-05 10:16:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,35 +2,43 @@ from os import environ
 import aiohttp
 from pyrogram import Client, filters
 
-API_ID = environ.get('API_ID')
-API_HASH = environ.get('API_HASH')
-BOT_TOKEN = environ.get('BOT_TOKEN')
-BITLY_KEY = environ.get('BITLY_KEY')
-
 bot = Client('Linkshortner',
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             BITLY_KEY=BITLY_KEY
+             api_id=os.environ["API_ID"],
+             api_hash=os.environ["API_HASH"],
+             bot_token=os.environ["BOT_TOKEN"]
 )
+
+BITLY_KEY=os.environ["BITLY_KEY"]
+
 @bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
-async def link_handler(bot, message):
-    link = message.matches[0].group(0)
+async def url(bot, update):
+    link = update.matches[0].group(0)
+    shortened_url, Err = get_shortlink(link)
+    if shortened_url is None:
+        message = f"Something went wrong \n{Err}"
+        await update.reply(message, quote=True)
+        return
+    message = f"Here is your shortlink\n {shortened_url}"
+    markup = InlineKeyboardMarkup([[InlineKeyboardButton("Link 🔗", url=shortened_url)]])
+    # i don't think this bot with get sending message error so no need of exceptions
+    await update.reply_text(text=message, reply_markup=markup, quote=True)
+
+def get_shortlink(url):
+    shortened_url = None
+    Err = None
     try:
-        short_link = await get_shortlink(link)
-        await message.reply(f'Here is your 👉 [link]({short_link})', quote=True)
-    except Exception as e:
-        await message.reply(f'Error: {e}', quote=True)
-
-
-async def get_shortlink(link):
-    url = 'https://gplinks.in/api'
-    params = {'api': API_KEY, 'url': link}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params, raise_for_status=True) as response:
-            data = await response.json()
-            return data["shortenedUrl"]
+       if BITLY_KEY:
+           ''' Bittly Shorten'''
+           s = Shortener(api_key=BITLY_KEY)
+           shortened_url = s.bitly.short(url)
+       else:
+           ''' Da.gd : I prefer this '''
+           s = Shortener()
+           shortened_url = s.dagd.short(url)
+    except Exception as error:
+        Err = f"#ERROR: {error}"
+        log.info(Err)
+    return shortened_url,Err
 
 
 bot.run()
